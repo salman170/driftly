35d4aa73d2091f52ed9941604ced13957fecf425
salman170 <salmanansari02120@gmail.com>
2023-11-02 17:33:19 +0530

    done with setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="text-green-500 ">
+        Hello ffsdf dfsdf dfdsf dfdsfa dfsdfsdf dfdfdf world
+      </div>
+    </>
+  );
+}
+
+export default App;
