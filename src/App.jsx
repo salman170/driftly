7ea657950ac8e6cd5d19bffaa2d470356504dea3
salman170 <salmanansari02120@gmail.com>
2023-11-06 15:58:57 +0530

    popup done

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,13 @@ import CarList from "./screen/Car List/CarList";
 import Finance from "./screen/Finance/Finance";
 import Sell from "./screen/Sell/Sell";
 import ExperienceCenter from "./screen/About/ExperienceCenter";
+import { useState } from "react";
+import ModalLoaction from "./others/ModalLoaction";
 
 function App() {
+  const [showMyModal, setShowMyModal] = useState(true);
+  const handleOnClose = () => setShowMyModal(false);
+
   return (
     <div>
       <div className="">
@@ -26,7 +31,11 @@ function App() {
           <Route exact path="/car-care" element={<CarCare />} />
           <Route exact path="/login" element={<Login />} />
           <Route exact path="/about" element={<About />} />
-          <Route exact path="/experience-center" element={<ExperienceCenter />} />
+          <Route
+            exact
+            path="/experience-center"
+            element={<ExperienceCenter />}
+          />
           <Route exact path="/car-lists" element={<CarList />} />
 
           <Route exact path="/sell" element={<Sell />} />
@@ -35,6 +44,7 @@ function App() {
           <Route exact path="/*" element={<NotFound />} />
         </Routes>
       </div>
+      <ModalLoaction onClose={handleOnClose} visible={showMyModal} />
     </div>
   );
 }
---
+++ src/others/ModalLoaction.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import Mumbai from "../assets/Home/Mumbai.png";
+import Bangalore from "../assets/Home/Bangalore.png";
+import NewDelhi from "../assets/Home/New Delhi.png";
+import Hyderabad from "../assets/Home/Hyderabad.png";
+import { AiOutlineClose } from "react-icons/ai";
+
+const ModalLoaction = ({ visible, onClose }) => {
+//   const handleOnClose = (e) => {
+//     if (e.target.id === "container") onClose();
+//   };
+  if (!visible) return null;
+
+  const data = [
+    {
+      img: Mumbai,
+      val: "Mumbai",
+    },
+    {
+      img: Bangalore,
+      val: "Bangalore",
+    },
+    {
+      img: Hyderabad,
+      val: "Hyderabad",
+    },
+    {
+      img: NewDelhi,
+      val: "NewDelhi",
+    },
+  ];
+  return (
+    <div
+      id="container"
+    //   onClick={handleOnClose}
+      className="fixed inset-0 z-40 flex flex-col items-center justify-center bg-black bg-opacity-60 backdrop-blur"
+    >
+      <div className="relative p-12 m-1 overflow-hidden text-center bg-white select-none lg:w-2/3 rounded-2xl">
+        <div className="pb-10 text-3xl">Select Location</div>
+        <div className="flex justify-center gap-8 ">
+          {data.map((x, i) => {
+            return (
+              <div key={i} className="">
+                <div className="px-8 py-6 mb-4 border border-[#D1D1D1] cursor-pointer rounded-xl hover:bg-[#E9E9E9] hover:shadow-lg">
+                  <div className="h-20 ">
+                    <img src={x.img} alt={x.val} srcset="" className="" />
+                  </div>
+                </div>
+                <div onClick={onClose} className="text-lg">
+                  {x.val}
+                </div>
+              </div>
+            );
+          })}
+        </div>
+        <div onClick={onClose} className="p-1.5  text-lg border rounded-full bg-[#f0f0f0] font-extralight w-min absolute top-4 right-4 cursor-pointer hover:shadow-lg">
+          <AiOutlineClose />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ModalLoaction;
