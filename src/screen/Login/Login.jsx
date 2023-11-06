2daa8ae0aa8785e2385ead97b9464d28f4a7529a
salman170 <salmanansari02120@gmail.com>
2023-11-06 18:04:27 +0530

    working on login page

--- src/screen/Login/Login.jsx
+++ src/screen/Login/Login.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className="h-screen w-full flex justify-center items-center">
+      <div className="flex shadow-xl rounded-3xl">
+        <div className="relative">
+          <img
+            src={require("../../assets/Login/LoginBackground.png")}
+            alt=""
+            srcSet=""
+          />
+          <img
+            src={require("../../assets/Logo/ColorLogo.png")}
+            alt=""
+            srcSet=""
+            className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+          />
+        </div>
+        <div className="py-16 px-10">
+          <div className="text-5xl font-extralight text-[#5E5E5E] mb-4">
+            Hello{" "}
+          </div>
+          <div className="text-[#5E5E5E] mb-4">
+            To keep connected with us please Log In with your <br />
+            Mobile Number and verify your identity via OTP.
+          </div>
+          <div>Mobile Number</div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
