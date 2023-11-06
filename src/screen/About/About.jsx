22a59dcc7027469d6c918c67b9d5fdab7c0d4bd0
iamharnad <haranathpriyan@gmail.com>
2023-11-06 17:40:36 +0530

    added footer

--- src/screen/About/About.jsx
+++ src/screen/About/About.jsx
@@ -28,6 +28,7 @@ const About = () => {
       <CustomerReview />
       <DCEC />
       <CardSlider />
+      <Footer />
     </div>
   );
 };
@@ -137,111 +138,113 @@ const ImageBanner = () => {
 
 const CardLayout = () => {
   return (
-    <div class='my-12 mx-auto px-4 md:px-12 bg-black text-white'>
-      <div class='flex flex-wrap -mx-1 lg:-mx-4'>
-        <div class='container mx-auto px-4 flex h-full mt-10'>
-          <div class='text-left text-white  lg:pt-2'>
-            <div class='mt-2 text-xs font-extralight tracking-tight md:text-4xl xl:text-m'>
-              Our Values
+    <>
+      <div class='my-12 mx-auto px-4 md:px-12 bg-black text-white'>
+        <div class='flex flex-wrap -mx-1 lg:-mx-4'>
+          <div class='container mx-auto px-4 flex h-full mt-10'>
+            <div class='text-left text-white  lg:pt-2'>
+              <div class='mt-2 text-xs font-extralight tracking-tight md:text-4xl xl:text-m'>
+                Our Values
+              </div>
+
+              <div className='p-2 text-xs'>
+                To give our customers the best car experience, <br></br>we stand
+                by our key values. We are:
+              </div>
             </div>
+          </div>
+          <div class='container mx-auto px-4 flex h-full mt-3 pl-1'>
+            <div class='my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/4'>
+              <article class='overflow-hidden'>
+                <a href='#'>
+                  <img
+                    alt='Placeholder'
+                    class='block h-auto w-full'
+                    src='https://images-saboomaruti-in.s3.ap-south-1.amazonaws.com/driftly/banners/image-1.webp'
+                  />
+                </a>
 
-            <div className='p-2 text-xs'>
-              To give our customers the best car experience, <br></br>we stand
-              by our key values. We are:
+                <a href='#' class='block max-w-sm pt-2'>
+                  <h5 class='text-xl font-normal tracking-tight'>
+                    Customer Obsessed
+                  </h5>
+                  <p class='font-light text-sm text-white'>
+                    We put the customer first in everything we do. We want to be
+                    famous for delivering the best experience and wowing our
+                    customers.
+                  </p>
+                </a>
+              </article>
             </div>
-          </div>
-        </div>
-        <div class='container mx-auto px-4 flex h-full mt-3 pl-1'>
-          <div class='my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/4'>
-            <article class='overflow-hidden'>
-              <a href='#'>
-                <img
-                  alt='Placeholder'
-                  class='block h-auto w-full'
-                  src='https://images-saboomaruti-in.s3.ap-south-1.amazonaws.com/driftly/banners/image-1.webp'
-                />
-              </a>
+            <div class='my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/4'>
+              <article class='overflow-hidden'>
+                <a href='#'>
+                  <img
+                    alt='Placeholder'
+                    class='block h-auto w-full'
+                    src='https://images-saboomaruti-in.s3.ap-south-1.amazonaws.com/driftly/banners/image-2.webp'
+                  />
+                </a>
 
-              <a href='#' class='block max-w-sm pt-2'>
-                <h5 class='text-xl font-normal tracking-tight'>
-                  Customer Obsessed
-                </h5>
-                <p class='font-light text-sm text-gray-700 dark:text-gray-400'>
-                  We put the customer first in everything we do. We want to be
-                  famous for delivering the best experience and wowing our
-                  customers.
-                </p>
-              </a>
-            </article>
-          </div>
-          <div class='my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/4'>
-            <article class='overflow-hidden'>
-              <a href='#'>
-                <img
-                  alt='Placeholder'
-                  class='block h-auto w-full'
-                  src='https://images-saboomaruti-in.s3.ap-south-1.amazonaws.com/driftly/banners/image-2.webp'
-                />
-              </a>
+                <a href='#' class='block max-w-sm pt-2'>
+                  <h5 class='mb-2 text-xl font-normal tracking-tight'>
+                    Fast Drivers
+                  </h5>
+                  <p class='font-light text-sm text-white '>
+                    We have an entrepreneurial passion for working at speed. We
+                    move fast and drive fast towards our goals.
+                  </p>
+                </a>
+              </article>
+            </div>
+            <div class='my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/4'>
+              <article class='overflow-hidden'>
+                <a href='#'>
+                  <img
+                    alt='Placeholder'
+                    class='block h-auto w-full'
+                    src='https://images-saboomaruti-in.s3.ap-south-1.amazonaws.com/driftly/banners/image-3.webp'
+                  />
+                </a>
 
-              <a href='#' class='block max-w-sm pt-2'>
-                <h5 class='mb-2 text-xl font-normal tracking-tight'>
-                  Fast Drivers
-                </h5>
-                <p class='font-light text-sm text-gray-700 dark:text-gray-400'>
-                  We have an entrepreneurial passion for working at speed. We
-                  move fast and drive fast towards our goals.
-                </p>
-              </a>
-            </article>
-          </div>
-          <div class='my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/4'>
-            <article class='overflow-hidden'>
-              <a href='#'>
-                <img
-                  alt='Placeholder'
-                  class='block h-auto w-full'
-                  src='https://images-saboomaruti-in.s3.ap-south-1.amazonaws.com/driftly/banners/image-3.webp'
-                />
-              </a>
+                <a href='#' class='block max-w-sm pt-2'>
+                  <h5 class='mb-2 text-xl font-normal tracking-tight'>
+                    Data Driven
+                  </h5>
+                  <p class='font-light text-sm text-white '>
+                    Data is a part of our DNA and drives all decision making.
+                    We're informed, results-driven and seek insights to help us
+                    improve and grow.
+                  </p>
+                </a>
+              </article>
+            </div>
+            <div class='my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/4'>
+              <article class='overflow-hidden'>
+                <a href='#'>
+                  <img
+                    alt='Placeholder'
+                    class='block h-auto w-full'
+                    src='https://images-saboomaruti-in.s3.ap-south-1.amazonaws.com/driftly/banners/image-4.webp'
+                  />
+                </a>
 
-              <a href='#' class='block max-w-sm pt-2'>
-                <h5 class='mb-2 text-xl font-normal tracking-tight'>
-                  Data Driven
-                </h5>
-                <p class='font-light text-sm text-gray-700 dark:text-gray-400'>
-                  Data is a part of our DNA and drives all decision making.
-                  We're informed, results-driven and seek insights to help us
-                  improve and grow.
-                </p>
-              </a>
-            </article>
-          </div>
-          <div class='my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/4'>
-            <article class='overflow-hidden'>
-              <a href='#'>
-                <img
-                  alt='Placeholder'
-                  class='block h-auto w-full'
-                  src='https://images-saboomaruti-in.s3.ap-south-1.amazonaws.com/driftly/banners/image-4.webp'
-                />
-              </a>
-
-              <a href='#' class='block max-w-sm pt-2'>
-                <h5 class='mb-2 text-xl font-normal tracking-tight'>
-                  Team Players
-                </h5>
-                <p class='font-light text-sm text-gray-700 dark:text-gray-400'>
-                  We're better as a team than as individuals. Everyone counts.
-                  We're here to have fun and win together on this exciting
-                  journey.
-                </p>
-              </a>
-            </article>
+                <a href='#' class='block max-w-sm pt-2'>
+                  <h5 class='mb-2 text-xl font-normal tracking-tight'>
+                    Team Players
+                  </h5>
+                  <p class='font-light text-sm text-white '>
+                    We're better as a team than as individuals. Everyone counts.
+                    We're here to have fun and win together on this exciting
+                    journey.
+                  </p>
+                </a>
+              </article>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
@@ -406,5 +409,143 @@ function CiustomerReviewSlider() {
     </>
   );
 }
+
+const Footer = () => {
+  return (
+    <>
+      <footer class=' text-center text-white bg-black  lg:text-left'>
+        <div className='container mx-auto'>
+          <div class='mx-6 py-10 text-left md:text-left'>
+            <div className='flex'>
+              <img
+                className='h-10 w-auto sm:h-16'
+                src='https://images-saboomaruti-in.s3.ap-south-1.amazonaws.com/driftly/banners/Driftly_Logo_Reverse.webp'
+                alt=''
+              />
+            </div>
+
+            <div class='grid-1 grid gap-1 md:grid-cols-2 lg:grid-cols-5 justify-between'>
+              <div class='w-30 p-2'>
+                <p className='font-thin'>
+                  If you need any help, we're available from{' '}
+                  <span className='font-bold'>8am-8pm, 7 days</span> a week
+                </p>
+                <br />
+                <p className='font-thin'>
+                  Call us on{' '}
+                  <span className='font-bold'>040&nbsp;2593&nbsp;5587</span>
+                </p>
+                <br />
+                <p className='font-thin'>
+                  Whatsapp us{' '}
+                  <span className='font-bold'>+91&nbsp;9817287234</span>
+                </p>
+              </div>
+
+              <div class='font-light w-40 m-auto'>
+                <p class='mb-4 flex justify-center uppercase md:justify-start'>
+                  Search Cars
+                </p>
+                <p class='mb-4'>
+                  <a href='#!' class='text-white '>
+                    Sell/Trade
+                  </a>
+                </p>
+                <p class='mb-4'>
+                  <a href='#!' class='text-white '>
+                    Financing
+                  </a>
+                </p>
+                <p class='mb-4'>
+                  <a href='#!' class='text-white '>
+                    Car Care
+                  </a>
+                </p>
+              </div>
+
+              <div class='font-light w-40 m-auto'>
+                <h6 class='mb-4 flex justify-center uppercase md:justify-start'>
+                  How it Works
+                </h6>
+                <p class='mb-4'>
+                  <a href='#!' class='text-white '>
+                    About Us
+                  </a>
+                </p>
+                <p class='mb-4'>
+                  <a href='#!' class='text-white '>
+                    Career
+                  </a>
+                </p>
+                <p class='mb-4'>
+                  <a href='#!' class='text-white '>
+                    Support
+                  </a>
+                </p>
+              </div>
+
+              <div class='font-light w-40 m-auto'>
+                <h6 class='mb-4 flex justify-center font-semibold uppercase md:justify-start'>
+                  FOLLOW US
+                </h6>
+                <p class='mb-4'>
+                  <a href='#!' class='text-white '>
+                    Facebook
+                  </a>
+                </p>
+                <p class='mb-4'>
+                  <a href='#!' class='text-white '>
+                    Twitter
+                  </a>
+                </p>
+                <p class='mb-4'>
+                  <a href='#!' class='text-white '>
+                    Instagram
+                  </a>
+                </p>
+              </div>
+
+              <div className='m-auto'>
+                <h6 class='mb-4 m-auto w-80 flex justify-center font-semibold uppercase md:justify-start'>
+                  Subscribe to our newsletter
+                </h6>
+                <div class=''>
+                  <div class='max-w-md mx-auto overflow-hidden md:max-w-xl'>
+                    <div class='md:flex'>
+                      <div class='w-full'>
+                        <div class='relative'>
+                          {' '}
+                          <input
+                            type='text'
+                            class='bg-white h-10 w-full px-4 pr-20 focus:outline-none hover:cursor-pointer'
+                            name=''
+                          />
+                          <button class='h-10 text-black absolute  text-2xl right-0 px-3 hover:text-[#0cf2b4] hover:scale-125'>
+                            <AiOutlineArrowRight />
+                          </button>{' '}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='border-t-2'></div>
+          <div class='flex items-center justify-center text-gray-400 border-neutral-200 p-6  lg:justify-between '>
+            <div class='mr-12 hidden lg:block'>
+              <span>©Driftly 2021. All rights reserved</span>
+            </div>
+
+            <div class='flex justify-center'>
+              <p className='mr-1'>Site map</p>|
+              <p className='ml-1'>Terms of Use</p>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
 
 export default About;
