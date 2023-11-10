d9b25cc8a40b8ae6fcb59083706ac60b154e64b3
iamharnad <haranathpriyan@gmail.com>
2023-11-10 09:55:18 +0530

    added experience page

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,44 @@
-import { Routes, Route } from "react-router-dom";
-import Home from "./screen/Home/Home";
+import { Routes, Route } from 'react-router-dom';
+import Home from './screen/Home/Home';
 
-import NotFound from "./others/NotFound";
-import CarDetails from "./screen/Car Details/CarDetails";
-import Support from "./screen/Support/Support";
-import HowItWorks from "./screen/How It Works/HowItWorks";
-import CarCare from "./screen/Car Care/CarCare";
-import Login from "./screen/Login/Login";
-import About from "./screen/About/About";
-import CarList from "./screen/Car List/CarList";
-import Finance from "./screen/Finance/Finance";
-import Sell from "./screen/Sell/Sell";
-import ExperienceCenter from "./screen/About/ExperienceCenter";
+import NotFound from './others/NotFound';
+import CarDetails from './screen/Car Details/CarDetails';
+import Support from './screen/Support/Support';
+import HowItWorks from './screen/How It Works/HowItWorks';
+import CarCare from './screen/Car Care/CarCare';
+import Login from './screen/Login/Login';
+import About from './screen/About/About';
+import CarList from './screen/Car List/CarList';
+import Finance from './screen/Finance/Finance';
+import Sell from './screen/Sell/Sell';
+import ExperienceCenter from './screen/About/ExperienceCenter';
+import Compare from './screen/Compare/Compare';
 
 function App() {
   return (
     <div>
-      <div className="">
+      <div className=''>
         {/* <div className="pt-[70px] lg:pt-20"> */}
         <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/car-details" element={<CarDetails />} />
-          <Route exact path="/support" element={<Support />} />
-          <Route exact path="/how-it-works" element={<HowItWorks />} />
-          <Route exact path="/car-care" element={<CarCare />} />
-          <Route exact path="/login" element={<Login />} />
-          <Route exact path="/about" element={<About />} />
-          <Route exact path="/experience-center" element={<ExperienceCenter />} />
-          <Route exact path="/car-lists" element={<CarList />} />
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/car-details' element={<CarDetails />} />
+          <Route exact path='/support' element={<Support />} />
+          <Route exact path='/how-it-works' element={<HowItWorks />} />
+          <Route exact path='/car-care' element={<CarCare />} />
+          <Route exact path='/login' element={<Login />} />
+          <Route exact path='/about' element={<About />} />
+          <Route
+            exact
+            path='/experience-center'
+            element={<ExperienceCenter />}
+          />
+          <Route exact path='/car-lists' element={<CarList />} />
 
-          <Route exact path="/sell" element={<Sell />} />
-          <Route exact path="/finance" element={<Finance />} />
+          <Route exact path='/sell' element={<Sell />} />
+          <Route exact path='/finance' element={<Finance />} />
 
-          <Route exact path="/*" element={<NotFound />} />
+          <Route exact path='/*' element={<NotFound />} />
+          <Route exact path='/compare' element={<Compare />} />
         </Routes>
       </div>
     </div>
---
+++ src/screen/Compare/Compare.js
@@ -0,0 +1,460 @@
+import React from 'react';
+import Header from '../../others/Header';
+import { Footer } from '../About/About';
+
+const Compare = () => {
+  return (
+    <>
+      <Header />
+      <CompareHero />
+      <Footer />
+    </>
+  );
+};
+
+const CompareHero = () => {
+  return (
+    <>
+      <div className='container mx-auto p-5'>
+        <nav class='bg-grey-light w-full rounded-md'>
+          <ol class='list-reset flex'>
+            <li>
+              <a
+                href='#'
+                class='text-primary transition duration-150 ease-in-out font-extralight'
+              >
+                All Cars
+              </a>
+            </li>
+            <li>
+              <span class='mx-2 text-neutral-500 dark:text-neutral-400'>
+                &gt;
+              </span>
+            </li>
+            <li>
+              <a href='#' class='text-[#0CF2B4]'>
+                Compare
+              </a>
+            </li>
+          </ol>
+        </nav>
+        {/* compare block */}
+        <div class=' text-black'>
+          <div class='flex flex-wrap -mx-1 lg:-mx-4'>
+            {/* <div class='container mx-auto px-4 flex h-full mt-10'></div> */}
+            {/* card 1 */}
+            <div class='my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/4 p-2'>
+              <div class='overflow-hidden'>
+                <div class='block max-w-sm pt-2'>
+                  <div class='text-4xl text-gray-500 font-extralight tracking-tight'>
+                    Compare Car Model
+                  </div>
+                </div>
+              </div>
+            </div>
+            {/* card 2 */}
+            <div class='my-1 w-full md:w-1/2 lg:my-4  lg:w-1/4 p-2'>
+              <div class='w-30 bg-white shadow rounded'>
+                {' '}
+                <div
+                  class='h-48 w-full bg-gray-200 flex flex-col justify-between p-4 bg-cover bg-center'
+                  style={{
+                    backgroundImage:
+                      "url('https://images.pexels.com/photos/7989741/pexels-photo-7989741.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940')",
+                  }}
+                >
+                  {' '}
+                  <div class='flex justify-between'>
+                    {' '}
+                    <input type='checkbox' />{' '}
+                    <button class='text-white hover:text-blue-500'>
+                      {' '}
+                      <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        class='h-6 w-6'
+                        fill='none'
+                        viewBox='0 0 24 24'
+                        stroke='currentColor'
+                      >
+                        {' '}
+                        <path
+                          stroke-linecap='round'
+                          stroke-linejoin='round'
+                          stroke-width='2'
+                          d='M12 4v16m8-8H4'
+                        />{' '}
+                      </svg>{' '}
+                    </button>{' '}
+                  </div>{' '}
+                  <div>
+                    {' '}
+                    <span class='uppercase text-xs bg-green-50 p-0.5 border-green-500 border rounded text-green-700 font-medium select-none'>
+                      {' '}
+                      available{' '}
+                    </span>{' '}
+                  </div>{' '}
+                </div>{' '}
+                <div class='p-4 flex flex-col items-left'>
+                  <h1 class='text-gray-800 text-start mt-1'>BMW X7</h1>{' '}
+                  <p class='text-gray-400 font-light text-xs text-left'>
+                    {' '}
+                    XDrive401 M Sport
+                  </p>{' '}
+                  <button className='p-2'>
+                    <svg
+                      width='180'
+                      height='48'
+                      viewBox='0 0 180 48'
+                      xmlns='http://www.w3.org/2000/svg'
+                    >
+                      <defs>
+                        <linearGradient
+                          id='linear-gradient'
+                          x1='0'
+                          y1='0.5'
+                          x2='1'
+                          y2='0.5'
+                        >
+                          <stop offset='0' stopColor='#0BF2B3' />
+                          <stop offset='1' stopColor='#00BAB8' />
+                        </linearGradient>
+                      </defs>
+                      <path
+                        d='M0,0H156a24,24,0,0,1,24,24v0a24,24,0,0,1-24,24H24A24,24,0,0,1,0,24V0A0,0,0,0,1,0,0Z'
+                        fill='url(#linear-gradient)'
+                      />
+                      <text
+                        x='50%'
+                        y='50%'
+                        fill='#fff'
+                        text-anchor='middle'
+                        alignment-baseline='middle'
+                        fontSize='14'
+                        dominant-baseline='middle'
+                      >
+                        <tspan x='50%' dy='0'>
+                          RESERVE
+                        </tspan>
+                      </text>
+                    </svg>
+                  </button>
+                  <div class='flex justify-between w-full mt-4'>
+                    {' '}
+                    <div class='flex items-center text-gray-500'>
+                      {' '}
+                      <input id='input1' type='checkbox' class='mr-2' />{' '}
+                      <label for='input1' class='select-none cursor-pointer'>
+                        Compare
+                      </label>{' '}
+                    </div>{' '}
+                    <div>
+                      {' '}
+                      <button class='py-1 px-4 bg-white text-gray-600 rounded hover:bg-gray-100 active:bg-gray-200 disabled:opacity-50 inline-flex items-center'>
+                        {' '}
+                        Add to List{' '}
+                        <svg
+                          xmlns='http://www.w3.org/2000/svg'
+                          class='h-4 w-4 ml-2'
+                          fill='none'
+                          viewBox='0 0 24 24'
+                          stroke='currentColor'
+                        >
+                          {' '}
+                          <path
+                            stroke-linecap='round'
+                            stroke-linejoin='round'
+                            stroke-width='2'
+                            d='M19 9l-7 7-7-7'
+                          />{' '}
+                        </svg>{' '}
+                      </button>{' '}
+                    </div>{' '}
+                  </div>{' '}
+                </div>{' '}
+              </div>
+            </div>
+
+            {/* card 3 */}
+            <div class='my-1 w-full md:w-1/2 lg:my-4  lg:w-1/4 p-2'>
+              <div class='w-30 bg-white shadow rounded'>
+                {' '}
+                <div
+                  class='h-48 w-full bg-gray-200 flex flex-col justify-between p-4 bg-cover bg-center'
+                  style={{
+                    backgroundImage:
+                      "url('https://images.pexels.com/photos/7989741/pexels-photo-7989741.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940')",
+                  }}
+                >
+                  {' '}
+                  <div class='flex justify-between'>
+                    {' '}
+                    <input type='checkbox' />{' '}
+                    <button class='text-white hover:text-blue-500'>
+                      {' '}
+                      <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        class='h-6 w-6'
+                        fill='none'
+                        viewBox='0 0 24 24'
+                        stroke='currentColor'
+                      >
+                        {' '}
+                        <path
+                          stroke-linecap='round'
+                          stroke-linejoin='round'
+                          stroke-width='2'
+                          d='M12 4v16m8-8H4'
+                        />{' '}
+                      </svg>{' '}
+                    </button>{' '}
+                  </div>{' '}
+                  <div>
+                    {' '}
+                    <span class='uppercase text-xs bg-green-50 p-0.5 border-green-500 border rounded text-green-700 font-medium select-none'>
+                      {' '}
+                      available{' '}
+                    </span>{' '}
+                  </div>{' '}
+                </div>{' '}
+                <div class='p-4 flex flex-col items-left'>
+                  <h1 class='text-gray-800 text-start mt-1'>BMW X7</h1>{' '}
+                  <p class='text-gray-400 font-light text-xs text-left'>
+                    {' '}
+                    XDrive401 M Sport
+                  </p>{' '}
+                  <button className='p-2'>
+                    <svg
+                      width='180'
+                      height='48'
+                      viewBox='0 0 180 48'
+                      xmlns='http://www.w3.org/2000/svg'
+                    >
+                      <defs>
+                        <linearGradient
+                          id='linear-gradient'
+                          x1='0'
+                          y1='0.5'
+                          x2='1'
+                          y2='0.5'
+                        >
+                          <stop offset='0' stopColor='#0BF2B3' />
+                          <stop offset='1' stopColor='#00BAB8' />
+                        </linearGradient>
+                      </defs>
+                      <path
+                        d='M0,0H156a24,24,0,0,1,24,24v0a24,24,0,0,1-24,24H24A24,24,0,0,1,0,24V0A0,0,0,0,1,0,0Z'
+                        fill='url(#linear-gradient)'
+                      />
+                      <text
+                        x='50%'
+                        y='50%'
+                        fill='#fff'
+                        text-anchor='middle'
+                        alignment-baseline='middle'
+                        fontSize='14'
+                        dominant-baseline='middle'
+                      >
+                        <tspan x='50%' dy='0'>
+                          RESERVE
+                        </tspan>
+                      </text>
+                    </svg>
+                  </button>
+                  <div class='flex justify-between w-full mt-4'>
+                    {' '}
+                    <div class='flex items-center text-gray-500'>
+                      {' '}
+                      <input id='input1' type='checkbox' class='mr-2' />{' '}
+                      <label for='input1' class='select-none cursor-pointer'>
+                        Compare
+                      </label>{' '}
+                    </div>{' '}
+                    <div>
+                      {' '}
+                      <button class='py-1 px-4 bg-white text-gray-600 rounded hover:bg-gray-100 active:bg-gray-200 disabled:opacity-50 inline-flex items-center'>
+                        {' '}
+                        Add to List{' '}
+                        <svg
+                          xmlns='http://www.w3.org/2000/svg'
+                          class='h-4 w-4 ml-2'
+                          fill='none'
+                          viewBox='0 0 24 24'
+                          stroke='currentColor'
+                        >
+                          {' '}
+                          <path
+                            stroke-linecap='round'
+                            stroke-linejoin='round'
+                            stroke-width='2'
+                            d='M19 9l-7 7-7-7'
+                          />{' '}
+                        </svg>{' '}
+                      </button>{' '}
+                    </div>{' '}
+                  </div>{' '}
+                </div>{' '}
+              </div>
+            </div>
+            {/* card 4 */}
+            <div class='my-1 w-full md:w-1/2 lg:my-4  lg:w-1/4 p-2'>
+              <div class='w-30 bg-white shadow rounded'>
+                {' '}
+                <div
+                  class='h-48 w-full bg-gray-200 flex flex-col justify-between p-4 bg-cover bg-center'
+                  style={{
+                    backgroundImage:
+                      "url('https://images.pexels.com/photos/7989741/pexels-photo-7989741.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940')",
+                  }}
+                >
+                  {' '}
+                  <div class='flex justify-between'>
+                    {' '}
+                    <input type='checkbox' />{' '}
+                    <button class='text-white hover:text-blue-500'>
+                      {' '}
+                      <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        class='h-6 w-6'
+                        fill='none'
+                        viewBox='0 0 24 24'
+                        stroke='currentColor'
+                      >
+                        {' '}
+                        <path
+                          stroke-linecap='round'
+                          stroke-linejoin='round'
+                          stroke-width='2'
+                          d='M12 4v16m8-8H4'
+                        />{' '}
+                      </svg>{' '}
+                    </button>{' '}
+                  </div>{' '}
+                  <div>
+                    {' '}
+                    <span class='uppercase text-xs bg-green-50 p-0.5 border-green-500 border rounded text-green-700 font-medium select-none'>
+                      {' '}
+                      available{' '}
+                    </span>{' '}
+                  </div>{' '}
+                </div>{' '}
+                <div class='p-4 flex flex-col items-left'>
+                  <h1 class='text-gray-800 text-start mt-1'>BMW X7</h1>{' '}
+                  <p class='text-gray-400 font-light text-xs text-left'>
+                    {' '}
+                    XDrive401 M Sport
+                  </p>{' '}
+                  <div className='justify-items-center'>
+                    {' '}
+                    <button className='p-2 justify-items-center'>
+                      <svg
+                        width='180'
+                        height='48'
+                        viewBox='0 0 180 48'
+                        xmlns='http://www.w3.org/2000/svg'
+                      >
+                        <defs>
+                          <linearGradient
+                            id='linear-gradient'
+                            x1='0'
+                            y1='0.5'
+                            x2='1'
+                            y2='0.5'
+                          >
+                            <stop offset='0' stopColor='#0BF2B3' />
+                            <stop offset='1' stopColor='#00BAB8' />
+                          </linearGradient>
+                        </defs>
+                        <path
+                          d='M0,0H156a24,24,0,0,1,24,24v0a24,24,0,0,1-24,24H24A24,24,0,0,1,0,24V0A0,0,0,0,1,0,0Z'
+                          fill='url(#linear-gradient)'
+                        />
+                        <text
+                          x='50%'
+                          y='50%'
+                          fill='#fff'
+                          text-anchor='middle'
+                          alignment-baseline='middle'
+                          fontSize='14'
+                          dominant-baseline='middle'
+                        >
+                          <tspan x='50%' dy='0'>
+                            RESERVE
+                          </tspan>
+                        </text>
+                      </svg>
+                    </button>
+                  </div>
+                  <div class='flex justify-between w-full mt-4'>
+                    {' '}
+                    <div class='flex items-center text-gray-500'>
+                      {' '}
+                      <input id='input1' type='checkbox' class='mr-2' />{' '}
+                      <label for='input1' class='select-none cursor-pointer'>
+                        Compare
+                      </label>{' '}
+                    </div>{' '}
+                    <div>
+                      {' '}
+                      <button class='py-1 px-4 bg-white text-gray-600 rounded hover:bg-gray-100 active:bg-gray-200 disabled:opacity-50 inline-flex items-center'>
+                        {' '}
+                        Add to List{' '}
+                        <svg
+                          xmlns='http://www.w3.org/2000/svg'
+                          class='h-4 w-4 ml-2'
+                          fill='none'
+                          viewBox='0 0 24 24'
+                          stroke='currentColor'
+                        >
+                          {' '}
+                          <path
+                            stroke-linecap='round'
+                            stroke-linejoin='round'
+                            stroke-width='2'
+                            d='M19 9l-7 7-7-7'
+                          />{' '}
+                        </svg>{' '}
+                      </button>{' '}
+                    </div>{' '}
+                  </div>{' '}
+                </div>{' '}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+const Card = () => {
+  return (
+    <div className='flex-none w-1/3 p-4'>
+      <div className='block rounded-lg bg-white shadow-lg dark:bg-neutral-700 text-left'>
+        <a href='#!'>
+          <img
+            className='rounded-t-lg'
+            src='https://tecdn.b-cdn.net/img/new/standard/nature/184.jpg'
+            alt=''
+          />
+        </a>
+        <div className='p-6'>
+          <h5 className='mb-2 text-xl font-bold tracking-wide text-neutral-800 dark:text-neutral-50'>
+            Explore the hidden beauty
+          </h5>
+          <p className='mb-2 text-base text-neutral-500 dark:text-neutral-300'>
+            Explore the captivating beauty of Antelope Canyon's red sandstone
+            formations and intricate play of light and shadows.
+          </p>
+          <a
+            href='#'
+            className='mt-3 inline-block rounded bg-blue-500 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-blue-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-blue-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-blue-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]'
+          >
+            Book now
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Compare;
